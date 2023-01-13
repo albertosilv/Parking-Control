222d9ef97095c7ad5d1238c2a3831f5b46054343
Alberto Silva <alberto.silva5456@gmail.com>
2023-01-13 03:41:58 -0300

    feat: Create delete  method in parking spot  in service

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -40,6 +40,8 @@ public class ParkingSpotService {
         return parkingSpotRepository.findById(id);
     }
 
+    @Transactional
     public void delete(ParkingSpotModel parkingSpotModel) {
+        parkingSpotRepository.delete(parkingSpotModel);
     }
 }
